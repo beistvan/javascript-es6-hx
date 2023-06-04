a4bf5a50d742771014fcbd03aaab7b7fb0e0f4fb
Beregszászi István <beistvan@gmail.com>
2023-06-04 22:05:47 +0200

    Constants

---
+++ consts.js
@@ -0,0 +1,7 @@
+const pi = 3.14;
+
+const weight = 65;
+const c = 300000000;
+const energy = weight * c ** 2;
+
+console.info(`Your body energy is ${energy} joules. This is 1 / ${Math.round(389000000000000000000 / energy)} of the yearly world energy consumption. Woah!`);
